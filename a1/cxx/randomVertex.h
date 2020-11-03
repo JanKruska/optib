eee29143591d1a0cf4996b70812dc04f6d89a62d
Jan Kruska <mortagh@download-privat.de>
2020-11-03 09:21:38 +0100

    Added C++ files, added graphs

---
+++ a1/cxx/randomVertex.h
@@ -0,0 +1,6 @@
+/* Nur Lesezugriff
+Änderungen in dieser Datei haben keine Auswirkung auf Auführung/Evaluation
+*/
+#include "types.h"
+
+std::pair<Vertex, Vertex> get_tuple_of_distinct_vertices(const Graph &g);
---
+++ a1/cxx/readLgf.h
@@ -0,0 +1,14 @@
+/* Nur Lesezugriff
+Änderungen in dieser Datei haben keine Auswirkung auf Auführung/Evaluation
+*/
+#include "types.h"
+
+// Convert edgeline to EdgeWithWeight
+EdgeWithWeight convertEdgeline(const std::string &input);
+
+// Read Graph from file in lemongraph format (.lgf)
+Graph readGraphFromFile(const std::string &filename);
+
+// Read Terminal Nodes for Steiner Tree from file
+std::vector<Vertex> readTerminalsFromFile(const std::string &filename,
+                                          const Graph &g);
