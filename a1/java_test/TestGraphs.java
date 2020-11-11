9a51e79d8704f3964e6cfed7fb445f72abb96fc7
Jan Kruska <mortagh@download-privat.de>
2020-11-11 15:52:00 +0100

    Added graph visualization

--- a1/java_test/TestGraphs.java
+++ a1/java_test/TestGraphs.java
@@ -1,16 +1,41 @@
+import com.mxgraph.layout.mxCircleLayout;
+import com.mxgraph.layout.mxIGraphLayout;
+import com.mxgraph.util.mxCellRenderer;
 import org.jgrapht.Graph;
+import org.jgrapht.ext.JGraphXAdapter;
 import org.jgrapht.graph.AsSubgraph;
 import org.jgrapht.graph.DefaultWeightedEdge;
 import org.junit.jupiter.api.RepeatedTest;
 import org.junit.jupiter.api.RepetitionInfo;
 
+import javax.imageio.ImageIO;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.Random;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 public class TestGraphs {
 
     String dir = "a1/cxx/";
+    String img_dir = "out/res";
+
+    public void saveImage(Graph<Integer, DefaultWeightedEdge> graph, String directory) throws IOException {
+        JGraphXAdapter<Integer, DefaultWeightedEdge> graphAdapter =
+                new JGraphXAdapter<>(graph);
+        mxIGraphLayout layout = new mxCircleLayout(graphAdapter);
+        layout.execute(graphAdapter.getDefaultParent());
+
+        BufferedImage image = mxCellRenderer.createBufferedImage(graphAdapter, null, 2, Color.WHITE, true, null);
+        File imgFile = new File(directory);
+        ImageIO.write(image, "PNG", imgFile);
+
+        assertTrue(imgFile.exists());
+    }
 
     @RepeatedTest(3)
     void mst(RepetitionInfo repetitionInfo) {
@@ -19,6 +44,11 @@ public class TestGraphs {
         Graph<Integer, DefaultWeightedEdge> graph = GraphReader.readGraph(dir+"Graph"+graphNumber+".lgf");
         AsSubgraph<Integer, DefaultWeightedEdge> tree = mySpanningTree.computeMST(graph);
         CheckerSpanningTree.checkSpanningTree(graph, tree);
+        try {
+            saveImage(tree,img_dir + "/graph_mst_"+graphNumber+".png");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     @RepeatedTest(3)
